af09d66915ba06ae4c31dc3c2f16544a9b12bb64
sane5ever <sane5ever@gmail.com>
2019-03-11 16:36:54 +0400

    HW2: parse xml via stax

--- src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -19,16 +19,29 @@ public class StaxStreamProcessor implements AutoCloseable {
         return reader;
     }
 
+    public String getAttribute(String name) throws XMLStreamException {
+        return reader.getAttributeValue(null, name);
+    }
+
     public boolean doUntil(int stopEvent, String value) throws XMLStreamException {
+        return doUntilAny(stopEvent, value) != null;
+    }
+
+
+
+    public String doUntilAny(int stopEvent, String... values) throws XMLStreamException {
         while (reader.hasNext()) {
             int event = reader.next();
             if (event == stopEvent) {
-                if (value.equals(getValue(event))) {
-                    return true;
+                String xmlValue = getValue(event);
+                for (String value : values) {
+                    if (value.equals(xmlValue)) {
+                        return xmlValue;
+                    }
                 }
             }
         }
-        return false;
+        return null;
     }
 
     public String getValue(int event) throws XMLStreamException {
@@ -53,4 +66,6 @@ public class StaxStreamProcessor implements AutoCloseable {
             }
         }
     }
+
+
 }
--- src/test/java/ru/javaops/masterjava/MainXml.java
+++ src/test/java/ru/javaops/masterjava/MainXml.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava;
 
+import com.google.common.base.Splitter;
 import com.google.common.io.Resources;
 import j2html.tags.ContainerTag;
 import one.util.streamex.StreamEx;
@@ -9,7 +10,10 @@ import ru.javaops.masterjava.xml.schema.Project;
 import ru.javaops.masterjava.xml.schema.User;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
@@ -19,8 +23,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+import static com.google.common.base.Strings.nullToEmpty;
 import static j2html.TagCreator.*;
 
 /**
@@ -31,9 +35,12 @@ import static j2html.TagCreator.*;
  * @link https://github.com/JavaOPs/topjava
  */
 public class MainXml {
+
+    public static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getValue).thenComparing(User::getEmail);
+
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.out.println("Format: projectName");
+            System.out.println("Required argument: projectName");
             System.exit(1);
         }
         String projectName = args[0];
@@ -43,13 +50,75 @@ public class MainXml {
 
         Set<User> users = main.parceByJaxb(projectName, payloadUrl);
         users.forEach(System.out::println);
-        
-        
+        System.out.println();
+
         String html = toHtml(users, projectName, Paths.get("out/usersJaxb.html"));
         System.out.println(html);
         try (Writer writer = Files.newBufferedWriter(Paths.get("out/users.html"))) {
             writer.write(html);
         }
+        System.out.println();
+        users = processByStax(projectName, payloadUrl);
+        users.forEach(System.out::println);
+    }
+
+    private static Set<User> processByStax(String projectName, URL payloadUrl) throws Exception {
+        try (InputStream is = payloadUrl.openStream()) {
+            StaxStreamProcessor processor = new StaxStreamProcessor(is);
+            final Set<String> groupNames = parseGroupNames(processor, projectName);
+            if (groupNames.isEmpty()) {
+                throw new IllegalArgumentException(String.format("Invalid project name %s or no groups", projectName));
+            }
+            return parseUsers(processor, groupNames);
+        }
+    }
+
+    private static Set<String> parseGroupNames(StaxStreamProcessor processor, String projectName) throws XMLStreamException {
+        final Set<String> result = new HashSet<>();
+        projects:
+        while (processor.doUntil(XMLEvent.START_ELEMENT, "Project")) {
+            if (projectName.equals(processor.getAttribute("name"))) {
+                for (String element = getNextStartElement(processor); element != null; element = getNextStartElement(processor)) {
+                    if (!element.equals("Group")) {
+                        break projects;
+                    }
+                    result.add(processor.getAttribute("name"));
+                }
+            }
+        }
+        return result;
+    }
+
+    private static Set<User> parseUsers(StaxStreamProcessor processor, Set<String> groupNames) throws XMLStreamException {
+        Set<User> result = new TreeSet<>(USER_COMPARATOR);
+        while (processor.doUntil(XMLEvent.START_ELEMENT, "User")) {
+            String groupRefs = processor.getAttribute("groupRefs");
+            if (!Collections.disjoint(groupNames, Splitter.on(' ').splitToList(nullToEmpty(groupRefs)))) {
+                User user = new User();
+                user.setEmail(processor.getAttribute("email"));
+                user.setValue(processor.getText());
+                result.add(user);
+            }
+        }
+        return result;
+    }
+
+    private static String getNextStartElement(StaxStreamProcessor processor) throws XMLStreamException {
+        return processor.doUntilAny(XMLEvent.START_ELEMENT, "Project", "Group", "Users");
+    }
+
+    private static String toHtml(Set<User> users, String projectName, Path path) throws IOException {
+        final ContainerTag table = table().with(tr().with(th("FullName"), th("email")))
+                .attr("border", "1")
+                .attr("cellspacing", "8")
+                .attr("cellspacing", "0");
+        users.forEach(u -> table.with(tr().with(td(u.getValue()), th(u.getEmail()))));
+        return html().with(
+                head().with(title(projectName + " users")),
+                body().with(h1(projectName + " users"), table)
+        ).render();
+
+
     }
 
     private Set<User> parceByJaxb(String projectName, URL payloadUrl) throws Exception {
@@ -65,24 +134,8 @@ public class MainXml {
             return StreamEx.of(payload.getUsers().getUser())
                     .filter(u -> !Collections.disjoint(groups, u.getGroupRefs()))
                     .collect(Collectors.toCollection(
-                            () -> new TreeSet<>(Comparator.comparing(User::getValue).thenComparing(User::getEmail)))
+                            () -> new TreeSet<>(USER_COMPARATOR))
                     );
         }
-    }
-
-    private static String toHtml(Set<User> users, String projectName, Path path) throws IOException {
-        final ContainerTag table = table().with(tr().with(th("FullName"), th("email")))
-                .attr("border", "1")
-                .attr("cellspacing", "8")
-                .attr("cellspacing", "0");
-        users.forEach(u -> table.with(tr().with(td(u.getValue()), th(u.getEmail()))));
-        return html().with(
-                head().with(title(projectName + " users")),
-                body().with(h1(projectName + " users"), table)
-        ).render();
-
-
-
-
     }
 }
