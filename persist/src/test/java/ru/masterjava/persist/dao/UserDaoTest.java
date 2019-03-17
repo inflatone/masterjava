b8428973b5e6043c4141f6dc53abdcc75d78bf02
sane5ever <sane5ever@gmail.com>
2019-03-17 13:27:10 +0400

    HW4: parse uploaded xml by chunk

--- persist/src/test/java/ru/masterjava/persist/dao/UserDaoTest.java
+++ persist/src/test/java/ru/masterjava/persist/dao/UserDaoTest.java
@@ -32,4 +32,11 @@ public class UserDaoTest extends AbstractDaoTest<UserDao>{
         List<User> users = dao.getWithLimit(5);
         assertEquals(FIST5_USERS, users);
     }
+
+    @Test
+    public void insertBatch() {
+        dao.clean();
+        dao.insertBatch(FIST5_USERS, 3);
+        assertEquals(5, dao.getWithLimit(100).size());
+    }
 }
--- web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/UserProcessor.java
@@ -1,5 +1,7 @@
 package ru.javaops.masterjava.upload;
 
+import ru.javaops.masterjava.persist.DBIProvider;
+import ru.javaops.masterjava.persist.dao.UserDao;
 import ru.javaops.masterjava.persist.model.User;
 import ru.javaops.masterjava.persist.model.UserFlag;
 import ru.javaops.masterjava.xml.schema.ObjectFactory;
@@ -16,8 +18,9 @@ import java.util.List;
 
 public class UserProcessor {
     private static final JaxbParser jaxbParser = new JaxbParser(ObjectFactory.class);
+    private static UserDao userDao = DBIProvider.getDao(UserDao.class);
 
-    public List<User> process(final InputStream is) throws XMLStreamException, JAXBException {
+    public List<User> process(final InputStream is, int chunkSize) throws XMLStreamException, JAXBException {
         final StaxStreamProcessor processor = new StaxStreamProcessor(is);
         List<User> users = new ArrayList<>();
         JaxbUnmarshaller unmarshaller = jaxbParser.createUnmarshaller();
@@ -26,6 +29,7 @@ public class UserProcessor {
                     = unmarshaller.unmarshal(processor.getReader(), ru.javaops.masterjava.xml.schema.User.class);
             users.add(new User(xmlUser.getValue(), xmlUser.getEmail(), UserFlag.valueOf(xmlUser.getFlag().value())));
         }
+        userDao.insertBatch(users, chunkSize);
         return users;
     }
 }
