b4d73e20c67209a554204607fe1e1084f68dc51f
sane5ever <sane5ever@gmail.com>
2019-04-03 15:11:41 +0400

    HW8: introduce attachments in webapp

--- web/webapp/src/main/java/ru/javaops/masterjava/webapp/SendServlet.java
+++ web/webapp/src/main/java/ru/javaops/masterjava/webapp/SendServlet.java
@@ -1,16 +1,21 @@
 package ru.javaops.masterjava.webapp;
 
+import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailWSClient;
+import ru.javaops.masterjava.service.mail.util.Attachments;
 
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+@MultipartConfig
 @WebServlet("/send")
 @Slf4j
 public class SendServlet extends HttpServlet {
@@ -24,7 +29,13 @@ public class SendServlet extends HttpServlet {
             String users = req.getParameter("users");
             String subject = req.getParameter("subject");
             String body = req.getParameter("body");
-            GroupResult groupResult = MailWSClient.sendBulk(MailWSClient.split(users), subject, body, null);
+            Part filePart = req.getPart("attach");
+            GroupResult groupResult = MailWSClient.sendBulk(
+                    MailWSClient.split(users), subject, body,
+                    filePart == null ? null : ImmutableList.of(
+                            Attachments.getAttachment(filePart.getSubmittedFileName(), filePart.getInputStream())
+                    )
+            );
             result = groupResult.toString();
             log.info("Processing finished with result: {}", result);
         } catch (Exception e) {
