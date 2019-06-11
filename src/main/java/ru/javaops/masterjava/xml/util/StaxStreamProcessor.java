be7f6970667f5a8693f78e840fb763499ca7778b
sane5ever <sane5ever@gmail.com>
2019-06-11 16:35:46 +0400

    introduce StAX stream processor (+ tests)

---
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -0,0 +1,55 @@
+package ru.javaops.masterjava.xml.util;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
+import java.io.InputStream;
+
+public class StaxStreamProcessor implements AutoCloseable {
+    private static final XMLInputFactory FACTORY = XMLInputFactory.newFactory();
+
+    private final XMLStreamReader reader;
+
+    public StaxStreamProcessor(InputStream in) throws XMLStreamException {
+        reader = FACTORY.createXMLStreamReader(in);
+    }
+
+    public XMLStreamReader getReader() {
+        return reader;
+    }
+
+    public boolean doUntil(int stopEvent, String value) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            if (event == stopEvent) {
+                if (value.equals(getValue(event))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public String getValue(int event) throws XMLStreamException {
+        return event == XMLEvent.CHARACTERS ? reader.getText() : reader.getLocalName();
+    }
+
+    public String getElementValue(String element) throws XMLStreamException {
+        return doUntil(XMLEvent.START_ELEMENT, element) ? reader.getElementText() : null;
+    }
+
+    public String getText() throws XMLStreamException {
+        return reader.getElementText();
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (XMLStreamException ignored) {
+            }
+        }
+    }
+}
