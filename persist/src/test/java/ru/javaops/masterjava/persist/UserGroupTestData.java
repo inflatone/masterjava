d5fc25bedf4a2fbfea27a1cb27f50030f92573c2
sane5ever <sane5ever@gmail.com>
2019-07-14 12:34:17 +0400

    fill new models' test data

---
+++ persist/src/test/java/ru/javaops/masterjava/persist/UserGroupTestData.java
@@ -0,0 +1,40 @@
+package ru.javaops.masterjava.persist;
+
+import ru.masterjava.persist.DBIProvider;
+import ru.masterjava.persist.dao.UserGroupDao;
+import ru.masterjava.persist.model.UserGroup;
+
+import java.util.List;
+import java.util.Set;
+
+import static ru.javaops.masterjava.persist.GroupTestData.*;
+import static ru.javaops.masterjava.persist.UserTestData.*;
+import static ru.masterjava.persist.dao.UserGroupDao.*;
+
+public class UserGroupTestData {
+    public static List<UserGroup> USER_GROUPS;
+
+    public static void init() {
+        UserTestData.init();
+        UserTestData.setUp();
+
+        GroupTestData.init();
+        GroupTestData.setUp();
+
+        USER_GROUPS = toUserGroups(ADMIN.getId(), TOPJAVA_07_ID, TOPJAVA_08_ID, MASTERJAVA_01_ID);
+        USER_GROUPS.addAll(toUserGroups(FULL_NAME.getId(), TOPJAVA_07_ID, MASTERJAVA_01_ID));
+        USER_GROUPS.addAll(toUserGroups(USER1.getId(), TOPJAVA_06_ID, MASTERJAVA_01_ID));
+        USER_GROUPS.addAll(toUserGroups(USER2.getId(), MASTERJAVA_01_ID));
+        USER_GROUPS.addAll(toUserGroups(USER3.getId(), MASTERJAVA_01_ID));
+    }
+
+    public static void setUp() {
+        UserGroupDao dao = DBIProvider.getDao(UserGroupDao.class);
+        dao.clean();
+        DBIProvider.getDBI().useTransaction((conn, status) -> dao.insertBatch(USER_GROUPS));
+    }
+
+    public static Set<Integer> getByGroupId(int groupId) {
+        return UserGroupDao.getByGroupId(groupId, USER_GROUPS);
+    }
+}
