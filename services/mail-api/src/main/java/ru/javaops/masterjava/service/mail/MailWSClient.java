ba7171e851c3e19e8e30060cc742e234e8b68d5f
sane5ever <sane5ever@gmail.com>
2019-07-24 09:10:40 +0400

    add MTOM (message transmission optimization mechanism)

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/MailWSClient.java
@@ -9,6 +9,7 @@ import ru.javaops.masterjava.web.WebStateException;
 import ru.javaops.masterjava.web.WsClient;
 
 import javax.xml.namespace.QName;
+import javax.xml.ws.soap.MTOMFeature;
 import java.util.List;
 import java.util.Set;
 
@@ -28,14 +29,14 @@ public class MailWSClient {
     public static String sendToGroup(final Set<Addressee> to, final Set<Addressee> cc, final String subject, final String body, final List<Attachment> attachments) throws WebStateException {
         log.info("Send mail to '" + to + "' cc '" + cc + "' subject '" + subject
                 + '\'' + (log.isDebugEnabled() ? "\nbody=" + body : ""));
-        String status = WS_CLIENT.getPort().sendToGroup(to, cc, subject, body, attachments);
+        String status = getPort().sendToGroup(to, cc, subject, body, attachments);
         log.info("Sent to group with status: " + status);
         return status;
     }
 
     public static GroupResult sendBulk(final Set<Addressee> to, final String subject, final String body, final List<Attachment> attachments) throws WebStateException {
         log.info("Send bulk to '" + to + "' subject '" + subject + '\'' + (log.isDebugEnabled() ? "\nbody=" + body : ""));
-        GroupResult result = WS_CLIENT.getPort().sendBulk(to, subject, body, attachments);
+        GroupResult result = getPort().sendBulk(to, subject, body, attachments);
         log.info("Sent bulk with result: " + result);
         return result;
     }
@@ -43,5 +44,9 @@ public class MailWSClient {
     public static Set<Addressee> split(String addressees) {
         Iterable<String> adds = Splitter.on(',').trimResults().omitEmptyStrings().split(addressees);
         return ImmutableSet.copyOf(Iterables.transform(adds, Addressee::new));
+    }
+
+    private static MailService getPort() {
+        return WS_CLIENT.getPort(new MTOMFeature(1024));
     }
 }
