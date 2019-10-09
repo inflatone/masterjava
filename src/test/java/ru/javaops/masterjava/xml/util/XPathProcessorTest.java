1b5b599b81d51559c9026de5819762a10b6886e7
sane5ever <sane5ever@gmail.com>
2019-10-09 08:02:31 +0400

    add xpath processor (+tests)

---
+++ src/test/java/ru/javaops/masterjava/xml/util/XPathProcessorTest.java
@@ -0,0 +1,32 @@
+package ru.javaops.masterjava.xml.util;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.NodeList;
+
+import javax.xml.xpath.XPathConstants;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
+import static com.google.common.io.Resources.getResource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XPathProcessorTest {
+    @Test
+    void getCities() throws Exception {
+        var buffer = new StringJoiner("\n");
+        try (var in = getResource("payload.xml").openStream()) {
+            var processor = new XPathProcessor(in);
+            var expression = XPathProcessor.getExpression(
+                    "/*[name()='Payload']/*[name()='Cities']/*[name()='City']/text()");
+            NodeList nodes = processor.evaluate(expression, XPathConstants.NODESET);
+            IntStream.range(0, nodes.getLength()).forEach(
+                    i -> buffer.add(nodes.item(i).getNodeValue())
+            );
+        }
+        var result = buffer.toString();
+        assertEquals("Санкт-Петербург\nКиев\nМинск", result);
+        System.out.println(result);
+    }
+
+
+}
