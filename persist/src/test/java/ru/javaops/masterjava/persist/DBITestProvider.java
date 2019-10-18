e78389468db5be1a0af9c6a328670d54736ef4a0
sane5ever <sane5ever@gmail.com>
2019-10-18 07:31:02 +0400

    add typesafe config support

--- persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
+++ persist/src/test/java/ru/javaops/masterjava/persist/DBITestProvider.java
@@ -1,13 +1,16 @@
 package ru.javaops.masterjava.persist;
 
+import ru.javaops.masterjava.config.Configs;
+
 import static java.sql.DriverManager.getConnection;
 
 public class DBITestProvider {
     public static void initDBI() {
-        initDBI("jdbc:postgresql://localhost:5432/masterjava", "postgres", "password");
+        var config = Configs.getConfig("persist.conf", "db");
+        initDBI(config.getString("url"), config.getString("user"), config.getString("password"));
     }
 
-    public static void initDBI(String dbUrl, String dbUser, String dbPassword) {
+    private static void initDBI(String dbUrl, String dbUser, String dbPassword) {
         DBIProvider.init(() -> {
             try {
                 Class.forName("org.postgresql.Driver");
