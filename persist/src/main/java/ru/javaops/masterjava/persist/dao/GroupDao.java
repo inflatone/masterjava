e4bbfa4df7619c7b39a0b3f504a4bc39e02e1115
sane5ever <sane5ever@gmail.com>
2019-10-25 07:27:14 +0400

    add project group processor

--- persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
+++ persist/src/main/java/ru/javaops/masterjava/persist/dao/GroupDao.java
@@ -9,6 +9,7 @@ import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapperFactory;
 import ru.javaops.masterjava.persist.model.Group;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -35,4 +36,7 @@ public abstract class GroupDao implements AbstractDao {
         int id = insertGeneratedId(group);
         group.setId(id);
     }
+
+    @SqlUpdate("INSERT INTO groups (name, type, project_id) VALUES (:name, CAST(:type AS group_type), :projectId)")
+    public abstract void insertBatch(@BindBean Collection<Group> groups);
 }
--- web/upload/src/main/java/ru/javaops/masterjava/upload/PayloadProcessor.java
+++ web/upload/src/main/java/ru/javaops/masterjava/upload/PayloadProcessor.java
@@ -2,6 +2,8 @@ package ru.javaops.masterjava.upload;
 
 import lombok.AllArgsConstructor;
 import lombok.val;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.util.JAXBParser;
 import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
 import javax.xml.bind.JAXBException;
@@ -10,6 +12,9 @@ import java.io.InputStream;
 import java.util.List;
 
 public class PayloadProcessor {
+    public static final JAXBParser jaxbParser = new JAXBParser(ObjectFactory.class);
+
+    private final ProjectGroupProcessor projectGroupProcessor = new ProjectGroupProcessor();
     private final UserProcessor userProcessor = new UserProcessor();
     private final CityProcessor cityProcessor = new CityProcessor();
 
@@ -26,7 +31,8 @@ public class PayloadProcessor {
 
     public List<FailedEmails> process(InputStream in, int chunkSize) throws XMLStreamException, JAXBException {
         val processor = new StaxStreamProcessor(in);
+        val groups = projectGroupProcessor.process(processor);
         val cities = cityProcessor.process(processor);
-        return userProcessor.process(processor, cities, chunkSize);
+        return userProcessor.process(processor, groups, cities, chunkSize);
     }
 }
