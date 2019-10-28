da5ef2a2e7e89dd8a7a45cdd559d567f75faf37e
sane5ever <sane5ever@gmail.com>
2019-10-28 07:24:05 +0400

    fix + resolve dependencies (ee libs on java 11)

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MainWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MainWSClientMain.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class MainWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendMail(
-                List.of(), List.of(), "Subject", "Body"
+                List.of(new Addressee("To <sane4ever@ya.ru>")),
+                List.of(),
+                "Java 11 API sending",
+                "is it okay?"
         );
     }
 }
